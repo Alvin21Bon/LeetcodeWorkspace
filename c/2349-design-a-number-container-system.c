e0e110aaaa5a44e08c336d8daa4e683681cbd260
Alvin21Bon <alvin21bon@gmail.com>
2025-02-14 03:00:50 -0500

    2/14/25

--- c/2349-design-a-number-container-system.c
+++ c/2349-design-a-number-container-system.c
@@ -1,44 +1,126 @@
 #include "leetcode.h"
 
-typedef struct {
+struct DynamicArray {
 	int* array;
-	size_t length;
+	int length;
+	int allocatedLength;
+};
+
+struct DynamicArray dynamicArray() 
+{
+	const int INITIAL_SIZE = 64;
+	struct DynamicArray array;
+	array.array = malloc(INITIAL_SIZE * sizeof(int));
+	array.allocatedLength = INITIAL_SIZE;
+	array.length = 0;
+
+	return array;
+}
+
+void arrayAdd(struct DynamicArray* array, const int num)
+{
+	if (array->length == array->allocatedLength)
+	{
+		array->allocatedLength *= 2;
+		array->array = realloc(array->array, array->allocatedLength * sizeof(int));
+	}
+
+	array->array[array->length] = num;
+	array->length++;
+}
+
+void arrayDeleteEnd(struct DynamicArray* array)
+{
+	array->length--;
+}
+
+void arraySwap(struct DynamicArray* array, const int idx1, const int idx2)
+{
+	int temp = array->array[idx1];
+	array->array[idx1] = array->array[idx2];
+	array->array[idx2] = temp;
+}
+
+void arrayDestroy(struct DynamicArray* array)
+{
+	free(array->array);
+	array->length = -1;
+	array->allocatedLength = 0;
+}
+
+// #############################################################
+
+typedef struct DynamicArray MinHeap;
+
+MinHeap minHeapInit()
+{
+	return dynamicArray();
+}
+
+int heapGetParentIdx(int childIdx) { return ((childIdx + 1) / 2) - 1; }
+int heapGetLeftChildIdx(int parentIdx) { return 2 * parentIdx + 1; }
+int heapGetRightChildIdx(int parentIdx) { return 2 * parentIdx + 2; }
+int* heapGetParent(MinHeap* heap, int childIdx)
+{
+	int parentIdx = heapGetParentIdx(childIdx);
+	if (parentIdx < 0) return NULL;
+
+	return &heap->array[parentIdx];
+}
+int* heapGetLeftChild(MinHeap* heap, int parentIdx)
+{
+	int childIdx = heapGetLeftChildIdx(parentIdx);
+	if (childIdx >= heap->length) return NULL;
+
+	return &heap->array[childIdx];
+}
+int* heapGetRightChild(MinHeap* heap, int parentIdx)
+{
+	int childIdx = heapGetRightChildIdx(parentIdx);
+	if (childIdx >= heap->length) return NULL;
+
+	return &heap->array[childIdx];
+}
+
+void minHeapify(MinHeap* heap, int rootIdx)
+{
+	int* leftChild = heapGetLeftChild(heap, rootIdx);
+	int* rightChild = heapGetRightChild(heap, rootIdx);
+	int rootValue = heap->array[rootIdx];
+	while (leftChild != NULL && rightChild != NULL)
+	{
+		int swapIdx;
+		int lowestValue;
+	} 
+}
+
+void buildMinHeap(MinHeap* heap)
+{
+
+}
+
+typedef struct {
+    
 } NumberContainers;
 
 
 NumberContainers* numberContainersCreate() 
 {
-	NumberContainers* numberContainer = malloc(sizeof(NumberContainers));
-	numberContainer->length = 1024;
-	numberContainer->array = malloc(numberContainer->length * sizeof(int));
-
-	return numberContainer;
+    
 }
 
 void numberContainersChange(NumberContainers* obj, int index, int number) 
 {
-	if ((size_t)index >= obj->length)
-	{
-		obj->length = index + 1024;
-		obj->array = realloc(obj->array, obj->length * sizeof(int));
-	}
-
-	obj->array[index] = number;
+    
 }
 
 int numberContainersFind(NumberContainers* obj, int number) 
 {
-	for (size_t i = 0; i < obj->length; i++)
-	{
-		if (obj->array[i] == number) return i;
-	}
-
-	return -1;
+    
 }
 
 void numberContainersFree(NumberContainers* obj) {
-	free(obj->array);
-	free(obj);
+    
 }
 
 /**

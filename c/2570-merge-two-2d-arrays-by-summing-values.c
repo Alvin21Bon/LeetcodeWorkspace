f7a134869cd094725318ae9dc277c4b2a8acf975
Alvin21Bon <alvin21bon@gmail.com>
2025-03-11 02:39:26 -0400

    this is why leetcode keeps running into heap-buffer-overflow when i am working with 2d array :/

--- c/2570-merge-two-2d-arrays-by-summing-values.c
+++ c/2570-merge-two-2d-arrays-by-summing-values.c
@@ -1,7 +1,5 @@
 #include "leetcode.h"
 
-// KEEPS RETURNING A HEAP OVERFLOW IN LEETCODE WHEN IT OUTPUTS FINE HERE. I DONT KNOW WHY.
-
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
@@ -10,8 +8,6 @@
 int** mergeArrays(int** nums1, int nums1Size, int* nums1ColSize, int** nums2, int nums2Size, int* nums2ColSize, int* returnSize, int** returnColumnSizes) 
 {
 	int** outputArray = malloc((nums1Size + nums2Size) * sizeof(int*));
-	*returnColumnSizes = malloc(sizeof(int));
-	**returnColumnSizes = 2;
 
 	int nums1Ptr = 0, nums2Ptr = 0, outputArrayPtr = 0;
 	while (nums1Ptr < nums1Size || nums2Ptr < nums2Size)
@@ -54,58 +50,9 @@ int** mergeArrays(int** nums1, int nums1Size, int* nums1ColSize, int** nums2, in
 	*returnSize = outputArrayPtr;
 	outputArray = realloc(outputArray, *returnSize * sizeof(int*));
 
+	*returnColumnSizes = malloc(*returnSize * sizeof(int));
+	for (int idx = 0; idx < *returnSize; idx++) (*returnColumnSizes)[idx] = 2;
+
 	return outputArray;
 }
 
-int main()
-{
-	int** nums1 = malloc(3 * sizeof(int*));
-	int** nums2 = malloc(3 * sizeof(int*));
-
-	for (int idx = 0; idx < 3; idx++)
-	{
-		nums1[idx] = malloc(2 * sizeof(int));
-		nums2[idx] = malloc(2 * sizeof(int));
-	}
-
-	nums1[0][0] = 1;
-	nums1[0][1] = 2;
-	nums1[1][0] = 2;
-	nums1[1][1] = 3;
-	nums1[2][0] = 4;
-	nums1[2][1] = 5;
-
-	nums2[0][0] = 1;
-	nums2[0][1] = 4;
-	nums2[1][0] = 3;
-	nums2[1][1] = 2;
-	nums2[2][0] = 4;
-	nums2[2][1] = 1;
-
-	int uh =2;
-	int ok;
-	int* fr;
-	int** frick = mergeArrays(nums1, 3, &uh, nums2, 3, &uh, &ok, &fr);
-
-	for (int idx = 0; idx < ok; idx++)
-	{
-		for (int j = 0; j < *fr; j++)
-		{
-			printf("%d ", frick[idx][j]);
-		}
-		printf("\n");
-	}
-
-	free(frick);
-
-
-	free(fr);
-	for (int idx = 0; idx < 3; idx++)
-	{
-		free(nums1[idx]);
-		free(nums2[idx]);
-	}
-	free(nums1);
-	free(nums2);
-	return 0;
-}
